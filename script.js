e90aec0e1cbedc01533656fe4872292a3e71a46c
Objetivos de Desarrollo Sostenible <july.perez.brendel@gmail.com>
2026-03-10 19:22:08 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const boton = document.getElementById('botonMagico');
+
+boton.onclick = function() {
+  alert('¡BOOM! Acabas de programar una alerta mágica.');
+  document.body.style.backgroundColor = 'yellow';
+};
